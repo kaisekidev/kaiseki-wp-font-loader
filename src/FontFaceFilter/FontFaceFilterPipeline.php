c8e11f3fa2ff63e6be88b3c63837882472f3f5b4
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-11-24 22:33:14 +0100

    implement filter pipelines

---
+++ src/FontFaceFilter/FontFaceFilterPipeline.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaiseki\WordPress\FontLoader\FontFaceFilter;
+
+use Kaiseki\WordPress\FontLoader\FontFaceInterface;
+
+final class FontFaceFilterPipeline implements FontFaceFilterInterface
+{
+    /** @var array<FontFaceFilterInterface> */
+    private array $filter;
+
+    public function __construct(FontFaceFilterInterface ...$filter)
+    {
+        $this->filter = $filter;
+    }
+
+    public function __invoke(FontFaceInterface $fontFace, string $filename): ?FontFaceInterface
+    {
+        foreach ($this->filter as $filter) {
+            if ($fontFace === null) {
+                return null;
+            }
+            $fontFace = ($filter)($fontFace, $filename);
+        }
+        return $fontFace;
+    }
+}
--- src/FontFaceRegistryFactory.php
+++ src/FontFaceRegistryFactory.php
@@ -6,21 +6,33 @@ namespace Kaiseki\WordPress\FontLoader;
 
 use Kaiseki\Config\Config;
 use Kaiseki\WordPress\FontLoader\FontFaceFilter\FontFaceFilterInterface;
+use Kaiseki\WordPress\FontLoader\FontFaceFilter\FontFaceFilterPipeline;
 use Kaiseki\WordPress\FontLoader\Loader\LoaderInterface;
 use Psr\Container\ContainerInterface;
 
+use function is_array;
+
+/**
+ * @phpstan-type FilterType class-string<FontFaceFilterInterface>|FontFaceFilterInterface
+ */
 final class FontFaceRegistryFactory
 {
     public function __invoke(
         ContainerInterface $container,
     ): FontFaceRegistry {
         $config = Config::get($container);
-        /** @var array<string, FontFaceFilterInterface> $filter */
-        $filter = $config->array('font_loader/filter', []);
+        /** @var array<string, FilterType|list<FilterType>> $filterConfig */
+        $filterConfig = $config->array('font_loader/filter', []);
+        /** @var array<string, FontFaceFilterInterface> $filters */
+        $filters = [];
+        foreach ($filterConfig as $key => $filter) {
+            $list = Config::initClassMap($container, is_array($filter) ? $filter : [$filter]);
+            $filters[$key] = new FontFaceFilterPipeline(...$list);
+        }
 
         return new FontFaceRegistry(
             $container->get(LoaderInterface::class),
-            $filter
+            $filters
         );
     }
 }
