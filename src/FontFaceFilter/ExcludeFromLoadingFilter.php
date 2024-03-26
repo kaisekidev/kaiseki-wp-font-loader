db4e208c18669afecdb7cd7bcdc5419e87e2032f
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-03-26 14:36:43 +0100

    fix class name

--- src/FontFaceFilter/ExcludeFromLoadingFilter.php
+++ src/FontFaceFilter/ExcludeFromLoadingFilter.php
@@ -6,7 +6,7 @@ namespace Kaiseki\WordPress\FontLoader\FontFaceFilter;
 
 use Kaiseki\WordPress\FontLoader\FontFaceInterface;
 
-final class PreloadFilter implements FontFaceFilterInterface
+final class ExcludeFromLoadingFilter implements FontFaceFilterInterface
 {
     public function __invoke(FontFaceInterface $fontFace, string $filename): ?FontFaceInterface
     {
